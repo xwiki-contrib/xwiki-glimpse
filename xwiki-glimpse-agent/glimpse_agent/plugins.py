ff8aadeae983a6074522515a2d65a0a98c42bc20
Fabio Mancinelli <fabio.mancinelli@xwiki.com>
2011-10-07 12:14:32 +0200

    Allow plugin files to have an extension
    The extension will not be part of the plugin name.

--- xwiki-glimpse-agent/glimpse_agent/plugins.py
+++ xwiki-glimpse-agent/glimpse_agent/plugins.py
@@ -22,15 +22,16 @@ def execute_plugin(plugin_path):
         data = plugin.stdout.read()
 
         result = {}
-        result['name'] = os.path.basename(plugin_path).replace('plugin_', '')      
+        filename = os.path.basename(plugin_path).replace('plugin_', '')
+        result['name'] = os.path.splitext(filename)[0]
 
         try:
             # Check if the output is in the JSON format and decode it
-            data = json.loads(data)            
+            data = json.loads(data)
 
             result['status'] = status
             result['properties'] = data
-        except ValueError:                        
+        except ValueError:
             result['properties'] = {}
             result['properties']['error'] = 'Plugin output not in JSON format'
             result['properties']['output'] = data
