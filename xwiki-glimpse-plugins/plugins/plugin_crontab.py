b104dc899595837e7aa9d29a5d7579fb4b0d7880
Guillaume Fenollar <guillaume.fenollar@xwiki.com>
2012-09-28 16:53:08 +0200

    add a line per user which has a non-empty crontab

--- xwiki-glimpse-plugins/plugins/plugin_crontab.py
+++ xwiki-glimpse-plugins/plugins/plugin_crontab.py
@@ -4,8 +4,12 @@ import json
 
 def get_crontab():
     cron = {}
-    cronfile = open('/var/spool/cron/crontabs/root','r')
-    cron['Cron'] = '\n'.join([line for line in cronfile.readlines() if not line.startswith('#')])
+
+    # every user that have a non-empty crontab
+    for user in os.listdir('/var/spool/cron/crontabs/'):
+        cronfile = open('/var/spool/cron/crontabs/%s' % (user),'r')
+        # crontab lines are already separated by a \n character, no need to add one
+	cron[user] = ''.join([line for line in cronfile.readlines() if not line.startswith('#')])
 
     return json.dumps(cron)
 
